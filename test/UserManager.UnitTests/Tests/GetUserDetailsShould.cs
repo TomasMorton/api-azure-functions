89abc8189e714341708b9cf779c5ab3370912a69
Tomas Morton <tomas.morton@broadsign.com>
2021-05-30 17:08:19 -0400

    [User manager] Return user details from API (#3)
    * Add helper to configure the function context
    
    * Return json containing user details

--- test/UserManager.UnitTests/Tests/GetUserDetailsShould.cs
+++ test/UserManager.UnitTests/Tests/GetUserDetailsShould.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Net;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
@@ -8,17 +9,17 @@ using Moq;
 using UserManager.Application;
 using Xunit;
 
-namespace UserManager.UnitTests
+namespace UserManager.UnitTests.Tests
 {
     public class GetUserDetailsShould
     {
-        private readonly Mock<FunctionContext> _context;
+        private readonly FunctionContext _context;
         private readonly Mock<IUserRepository> _userRepository;
 
         public GetUserDetailsShould()
         {
+            _context = new FunctionContextCreator().GetContext();
             _userRepository = new Mock<IUserRepository>(MockBehavior.Strict);
-            _context = new Mock<FunctionContext>(MockBehavior.Strict);
         }
 
         [Fact]
@@ -33,19 +34,20 @@ namespace UserManager.UnitTests
         }
 
         [Fact]
-        public async Task ReturnAWelcomeMessage()
+        public async Task ReturnTheNameProperty()
         {
+            var user = new UserDetails("id", "Bob the builder");
+            _userRepository.Setup(x => x.GetById(It.IsAny<string>())).ReturnsAsync(user);
             var request = CreateRequest("id=test");
-            AllowRetrievingUserDetails();
 
             var response = await RunFunction(request);
 
-            var responseText = ReadBody(response);
-            Assert.Contains("Welcome", responseText);
+            var responseDetails = ReadBody<UserDetailsResponse>(response);
+            Assert.Equal(user.UserName, responseDetails.name);
         }
 
         [Fact]
-        public async Task ReturnTheUsername()
+        public async Task ReturnTheIdProperty()
         {
             UserDetails user = new("id", "Bob the builder");
             _userRepository.Setup(x => x.GetById(It.IsAny<string>())).ReturnsAsync(user);
@@ -53,8 +55,8 @@ namespace UserManager.UnitTests
 
             var response = await RunFunction(request);
 
-            var responseText = ReadBody(response);
-            Assert.Contains(user.UserName, responseText);
+            var responseDetails = ReadBody<UserDetailsResponse>(response);
+            Assert.Equal(user.Id, responseDetails.id);
         }
 
         [Fact]
@@ -91,7 +93,7 @@ namespace UserManager.UnitTests
 
         private Task<HttpResponseData> RunFunction(HttpRequestData request)
         {
-            return new GetUserDetails(_userRepository.Object).Run(request, _context.Object);
+            return new GetUserDetails(_userRepository.Object).Run(request, _context);
         }
 
         private void AllowRetrievingUserDetails()
@@ -102,17 +104,23 @@ namespace UserManager.UnitTests
 
         private HttpRequestData CreateRequest(string queryString)
         {
-            var response = new MockHttpResponseData(_context.Object);
+            var response = new MockHttpResponseData(_context);
             return CreateRequest(queryString, response);
         }
 
         private HttpRequestData CreateRequest(string queryString, HttpResponseData response)
         {
-            var request = new Mock<HttpRequestData>(MockBehavior.Strict, _context.Object);
+            var request = new Mock<HttpRequestData>(MockBehavior.Strict, _context);
             var uri = new Uri($"http://test.function.com/GetUserDetails?{queryString}");
             request.Setup(x => x.Url).Returns(uri);
             request.Setup(x => x.CreateResponse()).Returns(response);
             return request.Object;
+        }
+
+        private T ReadBody<T>(HttpResponseData response)
+        {
+            var body = ReadBody(response);
+            return JsonSerializer.Deserialize<T>(body) ?? throw new Exception($"Failed to convert to json: {body}");
         }
 
         private string ReadBody(HttpResponseData response)
@@ -122,4 +130,6 @@ namespace UserManager.UnitTests
             return reader.ReadToEnd();
         }
     }
+
+    internal record UserDetailsResponse(string id, string name);
 }
